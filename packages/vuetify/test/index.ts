d7e19957b4b0a37d7088bc856bb9f8e9d76a567b
vladimir.ivanin <vladimir.ivanin@insales.ru>
2025-08-28 01:37:07 +0300

    Добавление текущей темы в конфигурацию тестов: определены основные цвета для темы, включая primary, secondary, accent, error, info, success и warning. Обновлены тесты для соответствия новым настройкам.

--- packages/vuetify/test/index.ts
+++ packages/vuetify/test/index.ts
@@ -27,6 +27,15 @@ config.global.mocks = {
         light: {},
         dark: {},
       },
+      currentTheme: {
+        primary: '#1976d2',
+        secondary: '#424242',
+        accent: '#82B1FF',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107',
+      },
     },
     rtl: false,
     breakpoint: {
